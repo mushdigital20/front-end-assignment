a4ad8921398c92825682256971a6e4a7b58dee39
Pedro Mello <pedro@mushdigital.com>
2015-08-26 09:23:34 +1000

    reset button

--- js/main.js
+++ js/main.js
@@ -1,63 +1,99 @@
 $(document).on('ready', function(){
-
-	//Loads the json file
-	loadData();
+	
+	var pageController = new PageController();
+	pageController.loadJsonData();
 });
 
+var PageController = function(){
+	this.startScreenObjects();
+};
 
-function loadData()
-{
-	$.ajax({
-		dataType: "json",
-		url: "js/data.json",
-		success: function(json){
-			
-			//Opens modal when the data has been read
-			openLightBox(function(){
-				startProgressBar(json.data.lightbox.duration);
+$.extend(PageController.prototype, {
+	    startScreenObjects:function(){
+	    	var self = this;
+	    	self.resetButton = $("#resetButton");
+	    	self.progressText = $("#progress-text");
+			self.completedIcon = $("#completed-icon");
+			self.progressBar = $("#progress-bar");
+			self.modal = $('#myModal');
+
+			//binds button click
+			self.resetButton.on('click',function(){
+				self.resetCounter(3);
 			});
+
+			this.resetScreenObjects();
+	    },
+	    //resets all objects to their initial state
+	    resetScreenObjects:function(){
+	    	this.progressBar.css("width","0%");
+	    	this.progressBar.removeClass("progress-bar-success").addClass("progress-bar-info");
+			this.completedIcon.hide();
+			this.progressText.html("Progress 0%");
+			this.resetButton.html("Reset");
+	    },
+	    //loads data from json file
+	    loadJsonData:function(){
+	    	this.changeCursorTo('wait');
+	    	var self = this;
+	    	$.ajax({
+				dataType: "json",
+				url: "js/data.json",
+				success: function(json){
+					//Opens modal when the data has been read
+					self.openLightBox(function(){
+						self.startProgressBar(json.data.lightbox.duration); //duration from json file
+					});
+				}
+			});
+	    },
+	    //Presents the lightbox and returns a callback when it's ready
+	    openLightBox:function(callback){
+	    	$(document).unbind('shown.bs.modal').on('shown.bs.modal', function(){
+				//Lighbox is presented
+				callback();
+			});
+			
+			this.modal.modal();
+	    },
+	    startProgressBar:function(duration){
+	    	duration = duration || 5000;
+	    	var self = this;
+	    	//Starts animation of the progressbar
+			self.progressBar.animate({width:'100%'}, {
+				duration:duration,
+			    specialEasing: {
+					width: "linear"
+				},
+				progress:function(animation, progress, remainingMs){
+					//updates the percentage text
+					self.progressText.html("Progress "+ parseInt(progress*100) +"%");
+				},
+				complete:function(){
+					// Animation complete.
+					self.changeCursorTo('default');
+					self.progressBar.removeClass("progress-bar-info").addClass("progress-bar-success");
+					self.completedIcon.show();
+					self.progressText.html("This task is 100% completed");
+				}	
+			});
+	    },
+	    resetCounter:function(secs){
+			var self = this;
+			if(secs > 0){
+				self.resetButton.html("Reseting in "+secs);
+				setTimeout(function(){
+					secs--;
+					self.resetCounter(secs);
+				},1000);
+			}else
+			{
+				self.resetScreenObjects();
+				self.loadJsonData();
+			}
 		},
-		error: function(a,b,c){
+		changeCursorTo:function(cursor){
+			$('body').css({'cursor' : cursor});
 		}
-	});
-}
-
-//Presents the lightbox and returns a callback when it's ready
-function openLightBox(callback)
-{
-	var modal = $('#myModal');
-
-	$(document).on('shown.bs.modal', function(){
-		//Lighbox is presented
-		callback();
-	});
-	
-	modal.modal();
-}
-
-//Animates the progressbar with the specified duration
-function startProgressBar(duration)
-{
-	duration = duration || 5000;
-	var progressText = $("#progress-text");
-	var completedIcon = $("#completed-icon");
-	var progressBar = $("#progress-bar");
-
-	//Starts animation of the progressbar
-	progressBar.animate({width:'100%'}, {
-		duration:duration,
-	    specialEasing: {
-			width: "linear"
-		},
-		progress:function(animation, progress, remainingMs){
-			//updates the percentage text
-			progressText.html("Progress "+ parseInt(progress*100) +"%");
-		},
-		complete:function(){
-			// Animation complete.
-			progressBar.removeClass("progress-bar-info").addClass("progress-bar-success");
-			completedIcon.show();
-			progressText.html("This task is 100% completed");
-		}	
-	});
-}
+	}
+);
